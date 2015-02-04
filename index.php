b2ae7b5dd36835f05b720dabccc0b467fb4f683c
unknown <capuanog@.clarionevents.local>
2015-02-04 16:03:55 +0000

    commit settings and ignore list for spirit

--- index.php
+++ index.php
@@ -85,3 +85,5 @@ $mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : ''
 $mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
 
 Mage::run($mageRunCode, $mageRunType);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
